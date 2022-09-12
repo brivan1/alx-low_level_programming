b3d2e97e76c76cc150f96ec88c80056668f2a64d
brivan1 <bkryan.bk@gmail.com>
2022-09-12 11:41:47 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main- serves as entry point
+ *
+ * Return:By default returns zero.
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return(0);
+}

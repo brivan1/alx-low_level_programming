9f388fe5d33200eea68d150b27a92dcc28e7df7c
brivan1 <bkryan.bk@gmail.com>
2022-09-15 01:26:38 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 - 9 using putchar
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+			

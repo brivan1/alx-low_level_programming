6e636d0b1b28db239c6f633c48725dba8d70347f
brivan1 <bkryan.bk@gmail.com>
2022-09-15 01:11:42 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -print from 0-9
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (; i <10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
+	

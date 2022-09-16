85a7a51fe66f0e0e72f9e412e08b1a15dc169d0a
brivan1 <bkryan.bk@gmail.com>
2022-09-16 08:07:34 -0700

    void-print_alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ * Description: prints alphabets
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
+	
+
+

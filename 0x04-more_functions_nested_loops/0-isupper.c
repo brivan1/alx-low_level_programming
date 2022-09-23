602412ec01367ac2d506eebaf92bcd2dcc991458
brivan1 <bkryan.bk@gmail.com>
2022-09-23 02:16:39 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,22 @@
 
 /**
  * _issuper - function that checks for uppercase character
+ * Description: uppercase character
  *@c: is the int that will use for the argument of the function
  * Return: 1 on success, zero if otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 'A'); (c <= 'Z')
-		return (1);
-	else
-		return (0);
+	char c;
+
+	for (c >= 'A'; c <= 'Z'; c++)
+	{
+		putchar (c);
+	}
+
+	putchar('\n');
+
+	return (1);
 }
+
+		

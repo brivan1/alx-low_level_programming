cc63bc614ad4278eccfcf5ead08a9604ea10c0a0
brivan1 <bkryan.bk@gmail.com>
2022-09-22 23:35:35 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * uppercase - function that checks for uppercase character
+ *
+ * Return: 1 on success, zero if otherwise
+ */
+int _isupper(int c)
+{
+

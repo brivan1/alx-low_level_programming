1e2907aebc06fb2b4b522e5a576408141e905671
brivan1 <bkryan.bk@gmail.com>
2022-09-22 23:38:13 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,6 +6,25 @@
  *
  * Return: 1 on success, zero if otherwise
  */
-int _isupper(int c)
+int main(void)
 {
+	{
+
+		    char c;
+
+
+
+		        c = 'A';
+
+			    printf("%c: %d\n", c, _isupper(c));
+
+			        c = 'a';
+
+				    printf("%c: %d\n", c, _isupper(c));
+
+				        return (0);
+
+	}
+
+}
 

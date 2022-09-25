6040dfa762b12be109e7498cd8dca0c5c35699dd
brivan1 <bkryan.bk@gmail.com>
2022-09-25 12:17:39 -0700

    prime factors

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints prime factors
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n = 612852475143;
+
+	int p;
+
+	while (p++ < n / 2)
+	{
+		if (n % p == 0)
+		{
+			n / = 2;
+			continue;
+		}
+		for (p = 3; p < n / 2; p += 2)
+		{
+			if (n % p == 0)
+			{
+				n /= p;
+			}
+		}
+	}
+	printf("%d\n", n);
+	return (0);
+}

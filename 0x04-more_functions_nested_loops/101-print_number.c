c40c3fa6b827b0bed7d27b5473e51c5702e77890
brivan1 <bkryan.bk@gmail.com>
2022-09-25 12:56:10 -0700

    print integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -10,6 +10,7 @@ void print_number(int n)
 {
 	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
 		_putchar(45);
@@ -17,6 +18,8 @@ void print_number(int n)
 	}
 
 	if (x / 10)
+	{
 		print_number(x / 10);
+	}
 	_putchar((x % 10) + '0');
 }

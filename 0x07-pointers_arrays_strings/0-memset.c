3b96bdc6b8114e7caea1da028af633c489f5d8db
brivan1 <bkryan.bk@gmail.com>
2022-09-29 11:46:57 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - fills the firt n bytes of the memory pointed to by s
+ * @s: pointer to memory
+ * @b: constant byte
+ * @n: space size
+ * Return: pointer to s
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s + i) = b;
+	}
+	return (s);
+}

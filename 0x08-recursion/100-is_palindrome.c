da684778e22cf1f199ec00dd20f93cb35814c061
brivan1 <bkryan.bk@gmail.com>
2022-10-10 02:06:24 -0700

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -16,3 +16,42 @@ int is_palindrome(char *s);
 		return (1);
 	return (check_palindrome(s));
 }
+
+/**
+ * check_palindrome - checks if a string is palindrome
+ * @s: the string to be checked
+ *
+ * Return: integer value
+ */
+int check_palindrome(char *s)
+{
+	int l = _strlen_recursion(s) - 1;
+
+	if (*s == s[l])
+	{
+		s++;
+		l--;
+	}
+	else
+	{
+		return (0);
+	}
+	return (1);
+}
+
+/**
+ * _strlen_recursion - get the length of a string
+ * @s: the string to acquire tthe length
+ *
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (_strlen_resursion(s) + 1);
+}

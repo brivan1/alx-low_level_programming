800a92a88d04837f93b16e4a2d050a64ddf53a07
brivan1 <bkryan.bk@gmail.com>
2022-10-20 13:19:16 -0700

    abs

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs- prints absolute value of an integer
+ * @i: integer
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		i = -i;
+	}
+	else if (i > 0)
+	{
+		i = i;
+	}
+	else
+	{
+		i = 0;
+	}
+	return (i);
+}

cc1836096300af22ada8933f2077b31eddf9c4c3
brivan1 <bkryan.bk@gmail.com>
2022-10-20 12:18:01 -0700

    puts

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts- prints a string
+ * @str: a param to put functions
+ * Return: 0
+ */
+
+void _puts(char *s)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}

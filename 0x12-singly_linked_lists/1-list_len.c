a092d29b84c4c0884a210df17541542aaae4c8f7
brivan1 <bkryan.bk@gmail.com>
2023-05-22 10:46:49 -0700

    returnelements

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,20 @@
+#include <stdlib.h>
 #include "lists.h"
+
 /**
- *list_len - return the number of elements in a linked
- *structure list
- *@h: the structure list
- *Return: number of elements in a linked structure list
+ * list_len - returns the number of elements in a linked structure list
+ * @h: pointer to the list_t structure list
+ *
+ * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-	size_t i = 0;
+	size_t n = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		n++;
 		h = h->next;
-		i++;
 	}
-	return (i);
+	return (n);
 }
